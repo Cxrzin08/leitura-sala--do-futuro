e4199348e8324bda44af1684a4f945a8223bfc6c
Cxrzin08 <felipepereirabritto08@gmail.com>
2025-08-25 19:07:16 -0300

    Cxrzin Fez A Boa

---
+++ main.py
@@ -0,0 +1,14 @@
+import random
+import pyautogui as pt
+import time
+
+time.sleep(4)
+
+pt.PAUSE = 0.3
+
+aleatorio = random.randint(60, 180)
+
+for i in range(20):
+ time.sleep(aleatorio)
+ pt.click(x=1837, y=530)
+
---
+++ pointer.py
@@ -0,0 +1,8 @@
+import random
+import pyautogui as pt
+import time as t
+
+t.sleep(5)
+
+teste = pt.position()
+print(teste)
